909abd2adb3e27772d4633e936e46491c087a230
jwdinius <josephwdinius@gmail.com>
2023-10-01 00:27:03 +0000

    Deploying to gh-pages from @ jwdinius/jwdinius.github.io@d7525100f8ed7f6f70ab4bf6c5e126fbb5833f12 🚀

--- assets/js/katex.js
+++ assets/js/katex.js
@@ -1,15 +1,1 @@
-$("script[type='math/tex']").replaceWith(
-  function(){
-    var tex = $(this).text();
-    return "<span class=\"inline-equation\">" +
-           katex.renderToString(tex) +
-           "</span>";
-});
-
-$("script[type='math/tex; mode=display']").replaceWith(
-  function(){
-    var tex = $(this).text();
-    return "<div class=\"equation\">" +
-           katex.renderToString("\\displaystyle "+tex) +
-           "</div>";
-});
+$("script[type='math/tex']").replaceWith(function(){var t=$(this).text();return'<span class="inline-equation">'+katex.renderToString(t)+"</span>"}),$("script[type='math/tex; mode=display']").replaceWith(function(){var t=$(this).text();return'<div class="equation">'+katex.renderToString("\\displaystyle "+t)+"</div>"});
